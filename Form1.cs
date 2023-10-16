a253a7d047001f99e660733add38f00bf1467ac1
Sami Ullah Saleem <samiullah1701734@gmail.com>
2023-10-17 02:39:32 +0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment_4
+{
+    public partial class Calculator : Form
+    {
+        private List<string> equationHistory = new List<string>();
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void Calculator_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (equation.Text == "Invalid equation" )
+            {
+                equation.Text = "";
+            }
+            equation.Text = equation.Text + "1";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (equation.Text == "Invalid equation" )
+            {
+                equation.Text = "";
+            }
+            equation.Text = equation.Text + "4";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (equation.Text == "Invalid equation" )
+            {
+                equation.Text = "";
+            }
+            equation.Text = equation.Text + "7";
+        }
+
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (equation.Text == "Invalid equation" )
+            {
+                equation.Text = "";
+            }
+            equation.Text = equation.Text + "8";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (equation.Text == "Invalid equation"  )
+            {
+                equation.Text = "";
+            }
+            equation.Text = equation.Text + "9";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (equation.Text == "Invalid equation" )
+            {
+                equation.Text = "";
+            }
+            equation.Text = equation.Text + "5";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (equation.Text == "Invalid equation" )
+            {
+                equation.Text = "";
+            }
+            equation.Text = equation.Text + "6";
+        }
+        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (equation.Text == "Invalid equation" )
+            {
+                equation.Text = "";
+            }
+            equation.Text = equation.Text + "2";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (equation.Text == "Invalid equation" )
+            {
+                equation.Text = "";
+            }
+            equation.Text = equation.Text + "3";
+        }
+
+        private void multiply(object sender, EventArgs e)
+        {
+            if (equation.Text == "Invalid equation" )
+            {
+                equation.Text = "";
+            }
+            equation.Text = equation.Text + "*";
+        }
+
+        private void add(object sender, EventArgs e)
+        {
+            if (equation.Text == "Invalid equation" )
+            {
+                equation.Text = "";
+            }
+            equation.Text = equation.Text + "+";
+        }
+
+        private void equal(object sender, EventArgs e)
+        {
+           
+            string equationToSolve = equation.Text;
+
+            
+            string result = EquationSolver.Solve(equationToSolve);
+
+            
+            equation.Text = result;
+            equationHistory.Add($"{equationToSolve} = {result}\n");
+
+            // Update the list view
+            listView1.Items.Clear();
+            foreach (string item in equationHistory)
+            {
+                listView1.Items.Add(item);
+            }
+
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            if (equation.Text == "Invalid equation" )
+            {
+                equation.Text = "";
+            }
+            equation.Text = equation.Text + "0";
+        }
+
+        private void point(object sender, EventArgs e)
+        {
+            if (equation.Text == "Invalid equation" )
+            {
+                equation.Text = "";
+            }
+            equation.Text = equation.Text + ".";
+        }
+
+        private void clear(object sender, EventArgs e)
+        {
+            if (equation.Text == "Invalid equation" )
+            {
+                equation.Text = "";
+            }
+            equation.Text = "";
+        }
+
+        private void delete(object sender, EventArgs e)
+        {
+            if (equation.Text == "Invalid equation" )
+            {
+                equation.Text = "";
+             }
+            else { equation.Text = equation.Text.Substring(0, equation.Text.Length - 1); }
+            
+        }
+
+        private void minus(object sender, EventArgs e)
+        {
+            if (equation.Text == "Invalid equation" )
+            {
+                equation.Text = "";
+            }
+            equation.Text = equation.Text + "-";
+        }
+
+        private void divide(object sender, EventArgs e)
+        {
+            if (equation.Text == "Invalid equation" )
+            {
+                equation.Text = "";
+            }
+            equation.Text = equation.Text + "/";
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                // Get the selected item's text
+                string selectedHistory = listView1.SelectedItems[0].Text;
+
+                // Set the equation text to the selected history
+                equation.Text = selectedHistory.Split('=')[0].Trim();
+            }
+        }
+
+        private void label1_click(object sender, EventArgs e) { }
+    }
+}
---
+++ calculator.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+class EquationSolver
+{
+    public static string Solve(string equation)
+    {
+        equation = equation.Replace(" ", "");
+        if (!IsValidEquation(equation))
+        {
+            return "Invalid equation";
+        }
+
+        try
+        {
+            // Replace double negations like "10--10" with a single negation
+            equation = equation.Replace("--", "+");
+            equation = equation.Replace("-+", "-");
+            equation = equation.Replace("++", "+");
+            equation = equation.Replace("+-", "-");
+            double result = EvaluateExpression(equation);
+            return result.ToString();
+        }
+        catch (Exception)
+        {
+            return "Invalid equation";
+        }
+    }
+
+    private static bool IsValidEquation(string equation)
+    {
+        // Allow digits, operators, parentheses, and optionally a leading negative sign
+        string pattern = @"^(-?\d+(\.\d+)?[\+\-\*/().\d]*)+$";
+        return Regex.IsMatch(equation, pattern);
+    }
+
+    private static double EvaluateExpression(string expression)
+    {
+        Stack<double> values = new Stack<double>();
+        Stack<char> operators = new Stack<char>();
+
+        for (int i = 0; i < expression.Length; i++)
+        {
+            if (expression[i] == '(')
+            {
+                int j = i;
+                int count = 1;
+                while (count > 0 && j < expression.Length - 1)
+                {
+                    j++;
+                    if (expression[j] == '(')
+                        count++;
+                    else if (expression[j] == ')')
+                        count--;
+                }
+                if (count != 0)
+                {
+                    throw new InvalidOperationException("Mismatched parentheses");
+                }
+                string subExpression = expression.Substring(i + 1, j - i - 1);
+                values.Push(EvaluateExpression(subExpression));
+                i = j;
+            }
+            else if (char.IsDigit(expression[i]) || (expression[i] == '-' && (i == 0 || expression[i - 1] == '(' || IsOperator(expression[i - 1]))))
+            {
+                int j = i;
+                while (j < expression.Length && (char.IsDigit(expression[j]) || expression[j] == '.'))
+                {
+                    j++;
+                }
+                string number = expression.Substring(i, j - i);
+                values.Push(double.Parse(number));
+                i = j - 1;
+            }
+            else if (IsOperator(expression[i]))
+            {
+                while (operators.Count > 0 && HasPrecedence(expression[i], operators.Peek()))
+                {
+                    double b = values.Pop();
+                    double a = values.Pop();
+                    char op = operators.Pop();
+                    values.Push(ApplyOperator(a, b, op));
+                }
+                operators.Push(expression[i]);
+            }
+        }
+
+        while (operators.Count > 0)
+        {
+            double b = values.Pop();
+            double a = values.Pop();
+            char op = operators.Pop();
+            values.Push(ApplyOperator(a, b, op));
+        }
+
+        if (values.Count == 1)
+        {
+            return values.Pop();
+        }
+        else
+        {
+            throw new InvalidOperationException("Invalid expression");
+        }
+    }
+
+
+    private static bool IsOperator(char c)
+    {
+        return c == '+' || c == '-' || c == '*' || c == '/';
+    }
+
+    private static bool HasPrecedence(char op1, char op2)
+    {
+        if ((op1 == '*' || op1 == '/') && (op2 == '+' || op2 == '-'))
+        {
+            return true;
+        }
+        if ((op1 == '/' && (op2 == '+' || op2 == '-')))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private static double ApplyOperator(double a, double b, char operatorSymbol)
+    {
+        switch (operatorSymbol)
+        {
+            case '+':
+                return a + b;
+            case '-':
+                return a - b;
+            case '*':
+                return a * b;
+            case '/':
+                if (b == 0)
+                {
+                    throw new DivideByZeroException();
+                }
+                return a / b;
+            default:
+                throw new InvalidOperationException("Invalid operator");
+        }
+    }
+}
